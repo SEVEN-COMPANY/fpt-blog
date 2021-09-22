f5bea579e9e3cdb8dce5fd97ca0662921642e1aa
tantruong2303 <new9a11516@gmail.com>
2021-09-22 20:56:14 +0700

    ADD - Calculate blog point service

--- FPTBlog/Src/BlogModule/Interface/IBlogService.cs
+++ FPTBlog/Src/BlogModule/Interface/IBlogService.cs
@@ -12,5 +12,6 @@ namespace FPTBlog.Src.BlogModule.Interface
         public List<Tag> GetTagFromBlog(Blog blog);
         public bool RemoveTagFromBlog(List<Tag> tags);
         public bool AddTagToBlog(Blog blog, List<Tag> tags);
+        public int CalculateBlogPoint(Blog blog);
     }
 }
