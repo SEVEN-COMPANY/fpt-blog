adc738c2c2f707355e353d23c41440b420ee0624
haicao2805 <haicao2805@gmail.com>
2021-10-19 15:04:38 +0700

    Refactor reward module

--- FPTBlog/Src/RewardModule/RewardApiController.cs
+++ FPTBlog/Src/RewardModule/RewardApiController.cs
@@ -1,14 +1,15 @@
-using System;
-using System.Collections.Generic;
 using FluentValidation.Results;
+
 using FPTBlog.Src.AuthModule;
 using FPTBlog.Src.RewardModule.DTO;
 using FPTBlog.Src.RewardModule.Entity;
 using FPTBlog.Src.RewardModule.Interface;
 using FPTBlog.Src.UserModule.Interface;
+
 using FPTBlog.Utils.Common;
 using FPTBlog.Utils.Interface;
 using FPTBlog.Utils.Locale;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace FPTBlog.Src.RewardModule {
