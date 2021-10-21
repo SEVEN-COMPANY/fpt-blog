e547dbcc8323d244793f2711479159154ee079ed
tantruong2303 <new9a11516@gmail.com>
2021-10-21 11:15:49 +0700

    ADD - Admin mvc chat

--- FPTBlog/Src/ChatModule/ChatAdminMvcController.cs
+++ FPTBlog/Src/ChatModule/ChatAdminMvcController.cs
@@ -1,7 +1,18 @@
-namespace FPTBlog.Src.ChatModule
-{
-    public class ChatAdminMvcController
-    {
-        
+using FPTBlog.Src.AuthModule;
+using FPTBlog.Src.ChatModule.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBlog.Src.ChatModule {
+    [Route("/admin/chat")]
+    [ServiceFilter(typeof(AuthGuard))]
+    public class ChatAdminMvcController : Controller {
+
+        private readonly IChatService ChatService;
+
+        public ChatAdminMvcController(IChatService chatService) {
+            this.ChatService = chatService;
+        }
+
+
     }
 }
